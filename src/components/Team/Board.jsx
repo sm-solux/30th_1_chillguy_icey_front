ff97dfff6b41b3e619d5772ffac1220e5cdb65cb
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-07-09 05:22:54 +0900

    Refactor. 팀 인원 수 표시 추가하였음.

--- src/components/Team/Board.jsx
+++ src/components/Team/Board.jsx
@@ -3,8 +3,11 @@ import "./Board.css";
 const Board = () => {
   return (
     <div className="Board-content">
-      <div className="team-name">우리팀</div>
-      <div className="team-board"></div>
+      <div className="board-team-box">
+        <div className="board-team-name">안녕하세요 제이름은</div>
+        <div className="board-team-number">|&nbsp;&nbsp;&nbsp;총 7명</div>
+      </div>
+      <div className="board-team-notice "></div>
     </div>
   );
 };
