31f446761bb7170efbd6b902c5634edcbd09e917
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-07-09 05:23:26 +0900

    Feat. 팀 생성 입력 UI 제작 완료

---
+++ src/components/Team/Teamcreate.jsx
@@ -0,0 +1,14 @@
+import "./Teamcreate.css";
+
+const Teamcreate = () => {
+  return (
+    <div className="team-create-content">
+      <div className="team-create-box">
+        <div className="team-create-mid-text">팀 이름</div>
+        <div className="team-create-input">직접 작성</div>
+      </div>
+    </div>
+  );
+};
+
+export default Teamcreate;
