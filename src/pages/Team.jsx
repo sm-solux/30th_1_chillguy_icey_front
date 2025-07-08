bee93d90efaca828ae0f8a10879b64d7e6d085ba
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-07-09 02:02:12 +0900

    Refator. 약속 확정 활성화 시 블럭 높이 수정 및 PromiseCheck 교체

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -3,6 +3,8 @@ import Board from "../components/Team/Board";
 import CardM from "../components/Team/CardM";
 import Massage from "../components/Team/Massage";
 import Promise from "../components/Team/Promise";
+import PromiseCheck from "../components/Team/PromiseCheck";
+
 import Teamlist from "../components/Team/Teamlist";
 
 const Team = () => {
@@ -25,11 +27,12 @@ const Team = () => {
 
       {/* 오른쪽 섹션 (작고 2개) */}
       <section className="Team-section2">
-        <div className="box team-promise-box">
-          <Promise />
+        <div className="box team-promise-box promExpanded">
+          {/* <Promise /> */}
+          <PromiseCheck />
           {/* 만약 활성화 되면, 여기에 약속 확정 컴포넌트 넣기 */}
         </div>
-        <div className="box team-list-box">
+        <div className="box team-list-box listShrinked">
           <Teamlist />
         </div>
       </section>
