cfe4b1c8276dab67b9951b24ff278bde49ae1190
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-07-13 04:39:37 +0900

    Feat. 팀장/팀원에 따른 약속 확정 버튼 배열 다르게 하는 기능

--- src/components/Team/PromiseCheck.jsx
+++ src/components/Team/PromiseCheck.jsx
@@ -4,26 +4,48 @@ import Promise from "./Promise.jsx";
 // import Promise_time from "./promise/Promise.jsx";
 import Button from "../Button.jsx";
 
-const PromiseCheck = () => {
-  return (
-    <div>
-      <Promise />
-      <div className={st.button_space}>
-        <div className={st.button_section}>
-          <Button text={"날짜"} type={"no"} />
-          <Button text={"시간"} />
+const PromiseCheck = ({ userType }) => {
+  if (userType === "Leader") {
+    return (
+      <div className={st.Promise_check_content}>
+        {/* <Promise /> */}
+        <div className={st.button_space}>
+          <div className={st.button_section}>
+            <Button text={"날짜"} type={"no"} />
+            <Button text={"시간"} />
+          </div>
+          <div className={st.button_section}>
+            <Button text={"약속 확정"} type={"promise-no"} />
+            <Button text={"완료"} />
+          </div>
         </div>
-        <div className={st.button_section}>
-          <Button text={"약속 확정"} type={"promise-no"} />
-          <Button text={"완료"} />
+        <div className={st.promise_check_box}>
+          {/* <Promise_date /> */}
+          {/* <Promise_time /> */}
         </div>
       </div>
-      <div className={st.promise_check_box}>
-        {/* <Promise_date /> */}
-        {/* <Promise_time /> */}
+    );
+  } else {
+    return (
+      <div className={st.Promise_check_content}>
+        {/* <Promise /> */}
+        <div className={st.button_space}>
+          <div className={st.button_section}>
+            {/* <Button text={"날짜"} type={"no"} /> */}
+            <Button text={"시간"} />
+          </div>
+          <div className={st.button_section}>
+            {/* <Button text={"약속 확정"} type={"promise-no"} /> */}
+            <Button text={"완료"} />
+          </div>
+        </div>
+        <div className={st.promise_check_box}>
+          {/* <Promise_date /> */}
+          {/* <Promise_time /> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default PromiseCheck;
