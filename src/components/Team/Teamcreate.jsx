d4778ea2eb4422261a754c9721c790c0201463f3
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-07-13 01:13:44 +0900

    Feat. Team 페이지 input 태그 수정 완료

--- src/components/Team/Teamcreate.jsx
+++ src/components/Team/Teamcreate.jsx
@@ -5,7 +5,12 @@ const Teamcreate = () => {
     <div className={st.team_create_content}>
       <div className={st.team_create_box}>
         <div className={st.team_create_mid_text}>팀 이름</div>
-        <div className={st.team_create_input}>직접 작성</div>
+        <input
+          className={st.team_create_input}
+          type="text"
+          placeholder="직접 입력"
+          maxLength="7" // 만약 글자수 제한 시 사용
+        />
       </div>
     </div>
   );
