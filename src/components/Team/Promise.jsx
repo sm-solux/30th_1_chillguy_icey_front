cf0f486f9e8a435b5b8118ae741c59b5e9a3404b
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-07-14 03:06:39 +0900

    Feat. 상단 달력 기능 추가 초안 완료

--- src/components/Team/Promise.jsx
+++ src/components/Team/Promise.jsx
@@ -1,12 +1,14 @@
 import st from "./Promise.module.css";
 import PromiseCalendar from "./promise/PromiseCalendar.jsx";
+import EXPromiseCalendar from "./promise/EXPromiseCalendar.jsx";
 
 const Promise = () => {
   return (
     <div className={st.Promise_content}>
       <div className={st.promise_calendar_box}>
         {/* 여기에 calendar 컴포넌트 (예: react-calendar) 삽입해도 좋아요 */}
-        <PromiseCalendar goalDate={"2025-07-22"} />
+        {/* <PromiseCalendar goalDate={"2025-07-22"} /> */}
+        <EXPromiseCalendar goalDate="2025-07-25" />
       </div>
 
       <div className={st.promise_text_box}>
