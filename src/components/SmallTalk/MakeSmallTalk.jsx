4c811bc8efcb0c9100541fa271522f41f8b47002
ssssuhhh <jshjune3590@gmail.com>
2025-07-17 04:38:44 +0900

    [#16] Feat: 스몰톡 페이지 추가
    스몰톡 페이지 요소들 추가
    + snow, listline svg 추가

---
+++ src/components/SmallTalk/MakeSmallTalk.jsx
@@ -0,0 +1,14 @@
+import st from "./MakeSmallTalk.module.css";
+const MakeSmallTalk = () => {
+  //이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+  };
+  return (
+    <button className={st.MakeSmallTalk} onClick={onClickButton}>
+      스몰톡 생성하기
+    </button>
+  );
+};
+
+export default MakeSmallTalk;
